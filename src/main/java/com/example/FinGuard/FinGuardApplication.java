f824a71f27b20b72afb6dfa3fafcde3495f5acb9
ayush120720 <ayush12072004@gmail.com>
2025-10-01 09:40:15 +0530

    files are pushed

---
+++ src/main/java/com/example/FinGuard/FinGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinGuardApplication.class, args);
+	}
+
+}
